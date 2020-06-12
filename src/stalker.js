537f3121da9bbac808863a728170e9ee5a854916
Bengin Cetindere <bengin@cetindere.de>
2020-06-12 17:36:49 +0200

    fix crash wrong url

--- src/stalker.js
+++ src/stalker.js
@@ -7,35 +7,37 @@ const stalkerInfo = {};
 
 // checks if the value has changed
 async function check(bot, stalker) {
-  let res;
+  let config;
   if (stalker.method === "post") {
-    res = await axios({
+    config = {
       url: stalker.url,
       method: "post",
       data: stalker.postBody,
       headers: {
         "Content-Type": stalker.contentType,
       },
-    }).catch(console.error);
+    };
   } else {
-    res = await axios({
+    config = {
       url: stalker.url,
       method: "get",
-    }).catch(console.error);
+    };
   }
-  const root = htmlParser.parse(res.data);
-  const newValue = root.querySelector(stalker.querySelector).innerHTML;
+  axios(config).then((res) => {
+    const root = htmlParser.parse(res.data);
+    const newValue = root.querySelector(stalker.querySelector).innerHTML;
 
-  // if new value should be sent instantly
-  if (!stalkerInfo[stalker._id].oldValue || stalkerInfo[stalker._id].oldValue !== newValue) {
-    bot.telegram.sendMessage(stalker.owner, `Old value was ${stalkerInfo[stalker._id].oldValue || "not defined"}\nNew Value is ${newValue}`);
-    stalkerInfo[stalker._id].oldValue = newValue;
-  } else if (!stalkerInfo[stalker._id].lastMessage
-     || Date.now() - stalkerInfo[stalker._id].lastMessage >= stalker.messageInterval * 1000 * 60) {
-    // then only send when messageInterval is done
-    bot.telegram.sendMessage(stalker.owner, `Value did not change, still ${newValue}`);
-    stalkerInfo[stalker._id].lastMessage = Date.now();
-  }
+    // if new value should be sent instantly
+    if (!stalkerInfo[stalker._id].oldValue || stalkerInfo[stalker._id].oldValue !== newValue) {
+      bot.telegram.sendMessage(stalker.owner, `Old value was ${stalkerInfo[stalker._id].oldValue || "not defined"}\nNew Value is ${newValue}`);
+      stalkerInfo[stalker._id].oldValue = newValue;
+    } else if (!stalkerInfo[stalker._id].lastMessage
+      || Date.now() - stalkerInfo[stalker._id].lastMessage >= stalker.messageInterval * 1000 * 60) {
+      // then only send when messageInterval is done
+      bot.telegram.sendMessage(stalker.owner, `Value did not change, still ${newValue}`);
+      stalkerInfo[stalker._id].lastMessage = Date.now();
+    }
+  }).catch(console.error);
 }
 
 // starts the checker function with setInterval
