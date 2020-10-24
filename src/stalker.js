022bd2df32617c5c2e558001b62bcec1bc789e93
Bengin Cetindere <bengin.cetindere@exxeta.com>
2020-10-24 11:07:20 +0200

    add user agent

--- src/stalker.js
+++ src/stalker.js
@@ -23,12 +23,16 @@ async function check(bot, stalker) {
       data: stalker.postBody,
       headers: {
         "Content-Type": stalker.contentType,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
       },
     };
   } else {
     config = {
       url: stalker.url,
       method: "get",
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
+      },
     };
   }
   axios(config).then((res) => {
